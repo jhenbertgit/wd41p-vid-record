7d7b50bc2a22d3429e22285aff95c8b8e830fecc
Jhenbert Villamucho <jvv205756@gmail.com>
2023-10-31 20:57:50 +0800

    added framer-motion

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 "use client";
 import dynamic from "next/dynamic";
 import { Col, Container, Row } from "reactstrap";
+import { motion, useScroll, useSpring } from "framer-motion";
+import classes from "./global.module.css";
 
 //import components
 import CustomHeader from "@/components/CustomHeader";
@@ -10,8 +12,15 @@ const VideoLists = dynamic(() => import("@/components/VideoLists"), {
 });
 
 export default function Home() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   return (
     <>
+      <motion.div className={classes["progress-bar"]} style={{ scaleX }} />
       <CustomHeader />
       <Container>
         <Row>
